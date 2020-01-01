d9480d0ba7fc3d82dc2d10298de6449546df59db
Bryan Kroesbeek <bryank.b96@gmail.com>
2020-01-01 22:31:31 +0100

    Add logic to determine the pixel color

--- Client/shaders/fragment.ts
+++ Client/shaders/fragment.ts
@@ -1,5 +1,6 @@
 export const fragmentSource = `
 #define MAX_SIZE 256
+#define PI 3.1415926
 
 precision mediump float;
 
@@ -14,6 +15,32 @@ uniform int effectCirclesSize;
 uniform int effectTrianglesSize;
 
 void main() {
+
+    // Effect circles
+    for (int i = 0; i < MAX_SIZE; i++) {
+        if (i >= effectCirclesSize) break;
+        bool isInRadius = pow(effectCircles[i].x - v_Pixelposition.x, 2.0) + pow(effectCircles[i].y - v_Pixelposition.y, 2.0) < pow(effectCircles[i].z, 2.0);
+        bool isInInner = pow(effectCircles[i].x - v_Pixelposition.x, 2.0) + pow(effectCircles[i].y - v_Pixelposition.y, 2.0) > pow(effectCircles[i].z - effectCircles[i].w, 2.0);
+
+        if (isInRadius && isInInner) {
+            gl_FragColor = activeColor;
+            return;
+        }
+    }
+
+    // Effect triangles
+    for (int i = 0; i < MAX_SIZE; i++) {
+        if (i >= effectTrianglesSize) break;
+
+        float angle = atan(effectTriangles[i].y - v_Pixelposition.y, effectTriangles[i].x - v_Pixelposition.x) + PI;
+        bool isInside = angle > effectTriangles[i].z && angle < effectTriangles[i].w;
+
+        if (isInside) {
+            gl_FragColor = activeColor;
+            return;
+        }
+    }
+
     gl_FragColor = baseColor;
 }
 `
