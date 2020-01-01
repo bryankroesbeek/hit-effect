2395204157a8314a48731c57eee544a57a133f1f
Bryan Kroesbeek <bryank.b96@gmail.com>
2020-01-01 20:41:26 +0100

    Remove canvas 2d context and rendering

--- Client/Components/EffectCanvas.tsx
+++ Client/Components/EffectCanvas.tsx
@@ -6,21 +6,16 @@ import { durationMillis, frameRate } from '../constants'
 type State = EffectCanvasState
 
 type EffectCanvasState = {
-    context: CanvasRenderingContext2D
-    diff: number
     effects: Effect[]
 }
 
 export class EffectCanvas extends React.Component<{}, State> {
     canvas: HTMLCanvasElement
-    pixelData: ImageData
 
     constructor(props: {}) {
         super(props)
 
         this.state = {
-            context: null,
-            diff: 0,
             effects: []
         }
     }
@@ -87,46 +82,6 @@ export class EffectCanvas extends React.Component<{}, State> {
 
     loop() {
         this.update()
-        let start = Date.now()
-        let effects = this.state.effects
-        let length = effects.length
-
-        let height = this.canvas.height
-        let width = this.canvas.width
-
-        let pixels = this.pixelData.data
-
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                let pixel = (y * width + x) * 4
-
-                let isInside: boolean = false
-                for (let i = 0; i < length; i++) {
-                    isInside = isInside || this.shouldRenderPixel(x, y, effects[i])
-                }
-                if (isInside) {
-                    pixels[pixel + 0] = 0
-                    pixels[pixel + 1] = 255
-                    pixels[pixel + 2] = 0
-                    pixels[pixel + 3] = 255
-                } else {
-                    pixels[pixel + 0] = 31
-                    pixels[pixel + 1] = 0
-                    pixels[pixel + 2] = 63
-                    pixels[pixel + 3] = 255
-
-                }
-            }
-        }
-        let end = Date.now()
-        this.setState({ diff: end - start })
-        this.state.context.putImageData(this.pixelData, 0, 0)
-        if (length < 1) return
-
-        if (end - effects[0].instanceTime > durationMillis) {
-            effects.shift()
-            this.setState({ effects: effects })
-        }
     }
 
     shouldRenderPixel(pixelX: number, pixelY: number, effect: Effect) {
@@ -146,15 +101,6 @@ export class EffectCanvas extends React.Component<{}, State> {
     }
 
     componentDidMount() {
-        let context = this.canvas.getContext("2d")
-        let data = context.createImageData(this.canvas.clientWidth, this.canvas.clientHeight)
-        this.setState({ context: context })
-        this.pixelData = data
-        this.context = context
-        let doc = document.getElementById("canvas-container")
-        this.canvas.width = doc.clientWidth
-        this.canvas.height = doc.clientHeight
-
         addEventListener("mousedown", this.handleClick)
         setInterval(() => this.loop(), frameRate)
     }
@@ -162,7 +108,6 @@ export class EffectCanvas extends React.Component<{}, State> {
     render() {
         return <div id="canvas-container">
             <canvas ref={ref => this.canvas = ref}></canvas>
-            <span>{this.state.diff}</span>
         </div>
     }
 }
