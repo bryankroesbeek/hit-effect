b2789fa5a1fd124313ce2c3fea526d8b4ec1fbfc
Bryan Kroesbeek <bryank.b96@gmail.com>
2020-01-01 23:55:59 +0100

    Flip y axis to match click position

--- Client/shaders/vertex.ts
+++ Client/shaders/vertex.ts
@@ -7,7 +7,7 @@ varying vec2 v_Pixelposition;
 void main() {
     vec2 clipSpace = ((a_position / resolution) * 2.0) - 1.0;
 
-    gl_Position = vec4(clipSpace, 0, 1);
+    gl_Position = vec4(clipSpace.x, clipSpace.y * -1.0, 0, 1);
     v_Pixelposition = a_position;
 }
 `
